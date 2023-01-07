b6ceb5beda0c63fa4eb3304dbc5df481c4315b84
saranshh <saranshchaurasia9@gmail.com>
2023-01-07 14:29:14 +0530

    Updates

---
+++ precache-manifest.4c122322ce7eb07060789d6fe3b7553e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9080d22ffd80bfb1e65d6c408eaf469b",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "bd944ea4a179057518bc",
+    "url": "/Keeper-app/static/js/2.dc8daa36.chunk.js"
+  },
+  {
+    "revision": "fe13d5f1210e9a304747",
+    "url": "/Keeper-app/static/js/main.1f64342a.chunk.js"
+  },
+  {
+    "revision": "9751fda007c6babce25f",
+    "url": "/Keeper-app/static/js/runtime-main.f8b8b1e7.js"
+  }
+]);
